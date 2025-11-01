d4f4f394b3fa0395602129fa686428a7e5c53a34
FOCKUSTY <116993925+FOCKUSTY@users.noreply.github.com>
2025-11-01 09:25:34 +0500

    -add anonim functions

--- src/index.ts
+++ src/index.ts
@@ -3,5 +3,7 @@ import "dotenv/config";
 import { login as loginTelegram } from "./telegram";
 import { login as loginBluesky } from "./bluesky";
 
-loginTelegram();
-loginBluesky();
+(async () => {
+  await loginBluesky();
+  await loginTelegram();
+})();
